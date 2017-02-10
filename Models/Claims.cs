f01b841ff6615f181f351b6abfcb815e9441ac72
Samuel Yun <samyun95@gmail.com>
2017-02-10 10:49:59 -0500

    Add models

---
+++ Models/Claims.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FisherInsurance.Models
+
+{
+    public class Quote
+    {
+        public int ID { get; set; }
+        public string Product { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
